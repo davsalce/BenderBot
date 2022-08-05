70062b434ce2142b02e8f59a88678c352da6699f
davsalce <d.salcedoort@gmail.com>
2022-08-05 15:53:34 +0200

    refactor: add CLU and CQA options to appsettings.Json

---
+++ Bot/CLU/CLUOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.CLU
+{
+
+    public class CLUOptions
+    {
+        public string Endpoint { get; set; }
+        public string Credential { get; set; }
+    }
+
+}
---
+++ Bot/CQA/CQAOptions.cs
@@ -0,0 +1,21 @@
+﻿namespace Bot.CQA
+{
+    public class CQAOptions
+    {
+        public Customquestionansweringclient CustomQuestionAnsweringClient { get; set; }
+        public Questionansweringproject QuestionAnsweringProject { get; set; }
+    }
+
+    public class Customquestionansweringclient
+    {
+        public string Endpoint { get; set; }
+        public string Credential { get; set; }
+    }
+
+    public class Questionansweringproject
+    {
+        public string ProyectName { get; set; }
+        public string DeploymentName { get; set; }
+    }
+
+}
