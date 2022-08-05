a5a079bf47e508bbcdbddd3751fe63a4a537b7f9
davsalce <d.salcedoort@gmail.com>
2022-08-05 15:40:02 +0200

    feat: CLU

--- Bot/Bots/BenderBot.cs
+++ Bot/Bots/BenderBot.cs
@@ -1,7 +1,10 @@
 ﻿using Azure;
+using Azure.AI.Language.Conversations;
 using Azure.AI.Language.QuestionAnswering;
+using Azure.Core;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using System.Text.Json;
 
 namespace Bot.Bots
 {
@@ -9,10 +12,12 @@ namespace Bot.Bots
     {
         private readonly QuestionAnsweringClient questionAnsweringClient;
         private readonly QuestionAnsweringProject questionAnsweringProject;
-        public BenderBot(QuestionAnsweringClient questionAnsweringClient, QuestionAnsweringProject questionAnsweringProject)
+        private readonly ConversationAnalysisClient conversationAnalysisClient;
+        public BenderBot(QuestionAnsweringClient questionAnsweringClient, QuestionAnsweringProject questionAnsweringProject, ConversationAnalysisClient conversationAnalysisClient)
         {
             this.questionAnsweringClient = questionAnsweringClient;
             this.questionAnsweringProject = questionAnsweringProject;
+            this.conversationAnalysisClient = conversationAnalysisClient;
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded,
@@ -24,21 +29,61 @@ namespace Bot.Bots
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             string textMessage = turnContext.Activity.Text;
+            var CLUrequestBody = new
+            {
+                analysisInput = new
+                {
+                    conversationItem = new
+                    {
+                        text = textMessage,
+                        id = turnContext.Activity.Id,
+                        participantId = turnContext.Activity.From.Id,
+                    }
+                },
+                parameters = new
+                {
+                    projectName = "ts-bot-CLU",
+                    deploymentName = "TSbotCLUdeployment",
 
-           
+                    // Use Utf16CodeUnit for strings in .NET.
+                    stringIndexType = "Utf16CodeUnit",
+                },
+                kind = "Conversation",
+            };
+            Response? response = await conversationAnalysisClient.AnalyzeConversationAsync(RequestContent.Create(CLUrequestBody));
+            if (response.ContentStream != null)
+            {
+                using JsonDocument result = JsonDocument.Parse(response.ContentStream);
+                JsonElement conversationalTaskResult = result.RootElement;
+                JsonElement conversationPrediction = conversationalTaskResult.GetProperty("result").GetProperty("prediction");
+
+
+
+                var intents = conversationPrediction.GetProperty("intents").Deserialize<List<Intent>>();
+                if (intents.Take(3).All(intent =>
+                    intent.category.Equals("RecomendSeries") ||
+                    intent.category.Equals("PendingEpisodes") ||
+                    intent.category.Equals("TrendingSeries"))) 
+                {
+                    await turnContext.SendActivityAsync(string.Join(", ", intents), cancellationToken: cancellationToken);
+                }
+            }
+
+
+
+
+
             AnswersOptions answersOptions = new AnswersOptions()
             {
-                ConfidenceThreshold = 0.5,
+                ConfidenceThreshold = 0.9,
                 IncludeUnstructuredSources = true,
                 ShortAnswerOptions = new ShortAnswerOptions()
                 {
-                    ConfidenceThreshold = 0.1,
-                    Size = 200,
-
+                    ConfidenceThreshold = 0.1
                 }
             };
 
-            Response<AnswersResult> customQuestionAnsweringResult = await this.questionAnsweringClient.GetAnswersAsync(textMessage, questionAnsweringProject);
+            Response<AnswersResult> customQuestionAnsweringResult = await this.questionAnsweringClient.GetAnswersAsync(textMessage, questionAnsweringProject, answersOptions);
             AnswersResult? answersResult = customQuestionAnsweringResult.Value;
             List<KnowledgeBaseAnswer>? knowledgeBaseAnswers = answersResult.Answers as List<KnowledgeBaseAnswer>;
 
@@ -54,4 +99,5 @@ namespace Bot.Bots
             }
         }
     }
+    public record Intent(string category, double confidenceScore);
 }
