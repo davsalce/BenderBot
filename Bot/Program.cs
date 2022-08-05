c9eeeca62b2454fd8e7c5b8d7e5abf650d3c5dbd
davsalce <108750943+davsalce@users.noreply.github.com>
2022-08-05 16:00:17 +0200

    Conversation language understanding (#2)
    * feat: CustomQuestionAnswering
    
    * refactor: add dependency injection on QuestionAnsweringClient
    
    * refactor: add dependency injection on QuestionAnsweringProject
    
    * chore:CustomQuestionAnswering
    
    * feat: CLU

--- Bot/Program.cs
+++ Bot/Program.cs
@@ -1,4 +1,5 @@
 using Azure;
+using Azure.AI.Language.Conversations;
 using Azure.AI.Language.QuestionAnswering;
 using Bot.Bots;
 using Microsoft.Bot.Builder;
@@ -8,7 +9,7 @@ WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddTransient<IBot, BenderBot>();
 builder.Services.AddTransient<IBotFrameworkHttpAdapter, CloudAdapter>();
-builder.Services.AddSingleton<QuestionAnsweringClient>(builder =>
+builder.Services.AddSingleton<QuestionAnsweringClient>(servicesProvider =>
 {
     QuestionAnsweringClientOptions questionAnsweringClientOptions = new QuestionAnsweringClientOptions()
     {
@@ -18,9 +19,16 @@ builder.Services.AddSingleton<QuestionAnsweringClient>(builder =>
     AzureKeyCredential azureKeyCredential = new AzureKeyCredential("08f8aae73b7a4049a0ba8f58187c3c67");
     return new QuestionAnsweringClient(uri, azureKeyCredential, questionAnsweringClientOptions);
 });
-builder.Services.AddSingleton<QuestionAnsweringProject>(builder =>
+builder.Services.AddSingleton<QuestionAnsweringProject>(servicesProvider =>
 {
     return new QuestionAnsweringProject("ts-bot-customQuestionAnswering", "production");
+
+});
+builder.Services.AddSingleton<ConversationAnalysisClient>(servicesProvider =>
+{ 
+    Uri endpoint = new Uri("https://ts-bot-language.cognitiveservices.azure.com");
+    AzureKeyCredential credential = new AzureKeyCredential("08f8aae73b7a4049a0ba8f58187c3c67"); 
+    return new ConversationAnalysisClient(endpoint, credential);
 
 });
 WebApplication? app = builder.Build();
