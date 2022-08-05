9744d30b76ca3d484fbdd41db98d2a90cc442f60
davsalce <108750943+davsalce@users.noreply.github.com>
2022-08-05 15:50:24 +0200

    Custom question answering (#1)
    * feat: CustomQuestionAnswering
    
    * refactor: add dependency injection on QuestionAnsweringClient
    
    * refactor: add dependency injection on QuestionAnsweringProject
    
    * chore:CustomQuestionAnswering

--- Bot/Bots/BenderBot.cs
+++ Bot/Bots/BenderBot.cs
@@ -1,22 +1,57 @@
-﻿using Microsoft.Bot.Builder;
+﻿using Azure;
+using Azure.AI.Language.QuestionAnswering;
+using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
 
 namespace Bot.Bots
 {
     public class BenderBot : ActivityHandler
     {
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, 
+        private readonly QuestionAnsweringClient questionAnsweringClient;
+        private readonly QuestionAnsweringProject questionAnsweringProject;
+        public BenderBot(QuestionAnsweringClient questionAnsweringClient, QuestionAnsweringProject questionAnsweringProject)
+        {
+            this.questionAnsweringClient = questionAnsweringClient;
+            this.questionAnsweringProject = questionAnsweringProject;
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded,
             ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
             await turnContext.SendActivityAsync("Hola", cancellationToken: cancellationToken);
         }
 
-        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext,
-            CancellationToken cancellationToken)
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             string textMessage = turnContext.Activity.Text;
-            await turnContext.SendActivityAsync(textMessage, cancellationToken: cancellationToken);
-        }
 
+           
+            AnswersOptions answersOptions = new AnswersOptions()
+            {
+                ConfidenceThreshold = 0.5,
+                IncludeUnstructuredSources = true,
+                ShortAnswerOptions = new ShortAnswerOptions()
+                {
+                    ConfidenceThreshold = 0.1,
+                    Size = 200,
+
+                }
+            };
+
+            Response<AnswersResult> customQuestionAnsweringResult = await this.questionAnsweringClient.GetAnswersAsync(textMessage, questionAnsweringProject);
+            AnswersResult? answersResult = customQuestionAnsweringResult.Value;
+            List<KnowledgeBaseAnswer>? knowledgeBaseAnswers = answersResult.Answers as List<KnowledgeBaseAnswer>;
+
+            if (knowledgeBaseAnswers != null && knowledgeBaseAnswers.Any())
+            {
+                KnowledgeBaseAnswer? knowledgeBaseAnswer = knowledgeBaseAnswers.FirstOrDefault();
+                string? text = knowledgeBaseAnswer?.ShortAnswer?.Text;
+                if (text == null)
+                {
+                    text = knowledgeBaseAnswer?.Answer;
+                }
+                await turnContext.SendActivityAsync(text, cancellationToken: cancellationToken);
+            }
+        }
     }
 }
