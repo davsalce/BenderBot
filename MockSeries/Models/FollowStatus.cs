43dea1f4db295c1e36c8b71f53b642ba4355e5e9
esalcedoo <elena@esalcedoo.com>
2022-08-09 16:39:53 +0200

    Feat: WIP MockSeriesClient

---
+++ MockSeries/Models/FollowStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockSeries.Models
+{
+    public enum FollowStatus
+    {
+
+        [System.Runtime.Serialization.EnumMember(Value = @"Following")]
+        Following = 0,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"WishList")]
+        WishList = 1,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"Abandoned")]
+        Abandoned = 2,
+
+        [System.Runtime.Serialization.EnumMember(Value = @"NotFollowing")]
+        NotFollowing = 3,
+
+    }
+}
