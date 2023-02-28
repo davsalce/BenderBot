e17a1b96a1cf96767f1e5b2915092fd055716ec0
esalcedoo <elena@esalcedoo.com>
2023-02-28 01:06:54 +0100

    WIP OpenAI

---
+++ Bot/OpenAI/OpenAIClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace Bot.OpenAI
+{
+    public class OpenAIClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly OpenAIOptions _options;
+
+        public OpenAIClient(HttpClient httpClient, OpenAIOptions options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+
+        public OpenAIClient(HttpClient httpClient, IOptions<OpenAIOptions> options)
+        {
+            _httpClient = httpClient;
+        }
+    }
+}
---
+++ Bot/OpenAI/OpenAIServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Options;
+using System.Net.Http;
+
+namespace Bot.OpenAI
+{
+    public static class OpenAIServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOpenAIClient(this IServiceCollection services, Action<OpenAIOptions> configureOptions)
+        {
+            // Add options for OpenAI using configuration action 
+            services.AddOptions<OpenAIOptions>().Configure(configureOptions);
+            // Add HTTP client for OpenAI using typed client pattern 
+            services.AddHttpClient<OpenAIClient>((sp, client) =>
+            {
+                var options = sp.GetRequiredService<IOptions<OpenAIOptions>>().Value;
+                client.BaseAddress = new Uri(options.Endpoint);
+
+                client.DefaultRequestHeaders.Add("api-key", options.ApiKey);
+                client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddOpenAIClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add options for OpenAI using configuration section 
+            services.AddOptions<OpenAIOptions>().Bind(configuration.GetSection("OpenAI")).ValidateDataAnnotations();
+            // Add HTTP client for OpenAI using typed client pattern 
+            services.AddHttpClient<OpenAIClient>((sp, client) =>
+            {
+                var options = sp.GetRequiredService<IOptions<OpenAIOptions>>().Value;
+                client.BaseAddress = new Uri(options.Endpoint);
+
+                client.DefaultRequestHeaders.Add("api-key", options.ApiKey);
+                client.DefaultRequestHeaders.Add("Content-Type", "application/json");
+            });
+            return services;
+        }
+    }
+}
